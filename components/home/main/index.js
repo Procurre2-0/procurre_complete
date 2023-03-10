86ea30c4c2409b2a17fce634de37b79eb39b1476
Krish Poptani <poptanikrish1@gmail.com>
2023-03-10 13:55:30 +0530

    home changes

--- components/home/main/index.js
+++ components/home/main/index.js
@@ -1,13 +1,8 @@
 import styles from "./styles.module.scss";
 import MainSwiper from "./swiper";
-import Offers from "./offers";
 import { useSession } from "next-auth/react";
-import Menu from "./Menu";
-import Link from "next/link";
-import { IoSettingsOutline } from "react-icons/io5";
-import { HiOutlineClipboardList } from "react-icons/hi";
-import { AiOutlineMessage } from "react-icons/ai";
-import { BsHeart } from "react-icons/bs";
+// import Menu from "./Menu";
+
 //-----------------------------
 import { useRef, useState } from "react";
 // Import Swiper React components
@@ -19,17 +14,13 @@ import "swiper/css/effect-cards";
 
 // import required modules
 import { EffectCards, Navigation } from "swiper";
-import User from "./User";
 import Header from "./Header";
 export default function Main() {
   const { data: session } = useSession();
   return (
     <div className={styles.main}>
       <Header />
-      <Menu />
       <MainSwiper />
-      <Offers />
-      <User />
     </div>
   );
 }
