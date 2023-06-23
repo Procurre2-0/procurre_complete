03b9adcfded47f9efeeb07211f6fe45aa7e3b6c7
Krish Poptani <poptanikrish1@gmail.com>
2023-06-23 16:05:12 +0530

    add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      script: "npm start",
+    },
+  ],
+
+  deploy: {
+    production: {
+      key: "key.pem",
+      user: "ubuntu",
+      host: "3.6.89.127",
+      ref: "origin/temp",
+      repo: "git@github.com:Procurre2-0/procurre_complete.git",
+      path: "/home/ubuntu",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "source ~/.nvm/nvm.sh &&npm install --force && npm run build  && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+      ssh_options: "ForwardAgent= yes",
+    },
+  },
+};
